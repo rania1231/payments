16d82d90d1993bf4cc3d2fade3ff6916a9e695f7
Rania Hamdi <130106344+rania1231@users.noreply.github.com>
2024-12-14 14:06:36 +0100

    Delete OneDrive/Documents/3eme/projects/project_spring_boot/Students and Payments Backend/backend-student-app/backend-student-app directory
    delete duplicated project

--- OneDrive/Documents/3eme/projects/project_spring_boot/Students and Payments Backend/backend-student-app/backend-student-app/src/main/java/enit/hamdi/backend_student_app/entities/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package enit.hamdi.backend_student_app.entities;
-
-public enum PaymentStatus {
-    CREATED, VALIDATED, REJECTED
-}
--- OneDrive/Documents/3eme/projects/project_spring_boot/Students and Payments Backend/backend-student-app/backend-student-app/src/main/java/enit/hamdi/backend_student_app/entities/PaymentType.java
+++
@@ -1,5 +0,0 @@
-package enit.hamdi.backend_student_app.entities;
-
-public enum PaymentType {
-    CASH, CHECK, TRANSFER, DEPOSIT
-}
